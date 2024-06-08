785f4618874c21183dd579fdb7b4bed7955929e1
KirillShakhov <kirill.shakhov14@gmail.com>
2024-06-08 15:48:21 +0300

    feat: lab3

--- cloud-terraform-k8s-config/get_kubeconfig.sh
+++ cloud-terraform-k8s-config/get_kubeconfig.sh
@@ -1,4 +1,9 @@
 yc managed-kubernetes cluster get-credentials --id cato2invbqt1khmn3u6i --external --kubeconfig ./kubeconfig
 
 kubectl --kubeconfig=$(pwd)/kubeconfig get pods -n ingress-nginx
-kubectl port-forward svc/argocd-server -n argocd 8080:443
+kubectl --kubeconfig=$(pwd)/kubeconfig port-forward svc/argocd-server -n argocd 8080:443
+
+kubectl --kubeconfig=$(pwd)/kubeconfig -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+kubectl port-forward service/grafana 3000:3000
+
+kubectl port-forward service/grafana 3000:3000
